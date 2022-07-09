c875dc287b17bc29f4211156ac84e9282ec521a6
Andrew Shaskin <shaskdev@gmail.com>
2022-07-08 18:54:43 -0600

    vanta added and embedded

---
+++ index.js
@@ -0,0 +1,15 @@
+<script src="p5.min.js"></script>
+<script src="vanta.topology.min.js"></script>
+<script>
+VANTA.TOPOLOGY({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x#ff5000
+})
+</script>
